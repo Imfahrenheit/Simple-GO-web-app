01ffdeaaea04c8fb95d412d9935df7368393ceab
Tareq Mahmud <tareq.mahmud@assemblin.fi>
2019-07-01 11:29:55 +0300

    simple rest api

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/imfahrenheit/simple-go-server
+
+go 1.12
+
+require (
+	github.com/gorilla/mux v1.7.2
+	github.com/lib/pq v1.1.1
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	// Import the gorilla/mux library we just installed
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+// The new router function creates the router and
+// returns it to us. We can now use this function
+// to instantiate and test the router outside of the main function
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// Declare the static file directory and point it to the
+	// directory we just made
+	staticFileDirectory := http.Dir("./assets/")
+	// Declare the handler, that routes requests to their respective filename.
+	// The fileserver is wrapped in the `stripPrefix` method, because we want to
+	// remove the "/assets/" prefix when looking for files.
+	// For example, if we type "/assets/index.html" in our browser, the file server
+	// will look for only "index.html" inside the directory declared above.
+	// If we did not strip the prefix, the file server would look for
+	// "./assets/assets/index.html", and yield an error
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	// The "PathPrefix" method acts as a matcher, and matches all routes starting
+	// with "/assets/", instead of the absolute route itself
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+
+	return r
+}
+
+func main() {
+	// The router is now formed by calling the `newRouter` constructor function
+	// that we defined above. The rest of the code stays the same
+	connString := "host=localhost port=5441  dbname=test_postgres sslmode=disable"
+	db, err := sql.Open("postgres", connString)
+
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+	InitStore(&dbStore{db: db})
+
+	r := newRouter()
+	http.ListenAndServe(":8080", RequestLogger(r))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+//logging is middleware for wrapping any handler we want to track response
+// times for and to see what resources are requested.
+
+// RequestLogger is the handler responsible for rending the index page for the site.
+func RequestLogger(targetMux http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		targetMux.ServeHTTP(w, r)
+
+		// log request by who(IP address)
+		requesterIP := r.RemoteAddr
+
+		log.Printf(
+			"%s\t\t%s\t\t%s\t\t%v",
+			r.Method,
+			r.RequestURI,
+			requesterIP,
+			time.Since(start),
+		)
+	})
+}
